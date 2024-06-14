9175b8e632529aa9d6190033c32a01d7c8acc7ea
kurzzt <87642181+kurzzt@users.noreply.github.com>
2024-06-14 17:31:59 +0700

    remove unecessary package

--- src/routes/v1/unprotected/auth/signin.ts
+++ src/routes/v1/unprotected/auth/signin.ts
@@ -4,8 +4,8 @@ import { loginDto } from "../../../../models/auth";
 import { User } from "../../../../models/user";
 import jwt from "jsonwebtoken";
 import * as bcrypt from 'bcrypt';
-import httpStatus from "http-status";
 import { config } from "../../../../config/config";
+import { StatusCodes } from "http-status-codes";
 
 export const POST: RequestHandler[] = [
   validateData(loginDto),
@@ -13,11 +13,11 @@ export const POST: RequestHandler[] = [
     const { email, password } = req.body
     const data = await User.findOne({ email }, '+password')
     if(!data) {
-      res.status(httpStatus.FORBIDDEN).send({ message: 'Email or Password incorrect' })
+      res.status(StatusCodes.FORBIDDEN).send({ message: 'Email or Password incorrect' })
     }else {
       const correct = await bcrypt.compare(password, data.password)
       const token = jwt.sign({ sub: data['_id'] }, config.JWT_SECRET, { expiresIn: "1h" })
-      if (!correct) res.status(httpStatus.FORBIDDEN).send({ message: 'Email or Password incorrect' })
+      if (!correct) res.status(StatusCodes.FORBIDDEN).send({ message: 'Email or Password incorrect' })
       else res.status(200).send({ data, token })
     }
   }
